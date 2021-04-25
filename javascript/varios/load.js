525f182d264494b03513aac95acc405f9fd882a3
Carlos Rayon <carlosrayon.developer@gmail.com>
2021-04-25 23:20:08 +0200

    add new load js

--- javascript/varios/load.js
+++ javascript/varios/load.js
@@ -1,3 +1,7 @@
 window.addEventListener('load', function () {
     console.log('Load all document');
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Load all document');
+});
