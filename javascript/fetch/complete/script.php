988a83c5c7b5d89b077a0c1533437d9b85241fbc
Carlos Rayon <carlosrayon.developer@gmail.com>
2022-02-17 11:38:06 +0100

    fix: normalize response ajax

--- javascript/fetch/complete/script.php
+++ javascript/fetch/complete/script.php
@@ -3,5 +3,15 @@
 
 // print_r($_POST);
 
+echo json_encode(
+    [
+        'code' => 200,
+        'msg' => 'success',
+        'data' => [
+            'data-1' => 'xxxxx',
+            'data-2' => 'yyyyy',
+        ]
+    ]
+);
 echo json_encode(['status' => true, 'code' => 'ok', 'msg' => 'all ok']);
 // echo json_encode(['status' => true, 'code' => 'ok', 'msg' => 'all ok'],JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
--- jquery/ajax/script.php
+++ jquery/ajax/script.php
@@ -1,8 +1,16 @@
 <?php
 
 $response = array(
-'data' => 'hello',
-'data-dos' => 'world',
+    'data' => 'hello',
+    'data-dos' => 'world',
 );
 
+$response = [
+    'code' => 200,
+    'msg' => 'success',
+    'data' => [
+        'data-1' => 'xxxxx',
+        'data-2' => 'yyyyy',
+    ]
+];
 echo json_encode($response, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
